3ed3a69312bfc95455597c96c5b0708b0b444551
farhan <faliefs@yahoo.com>
2016-05-17 11:02:04 +0700

    first commmit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 Test Coba</div>
+                <div class="title">Laravel 5 Test Github</div>
             </div>
         </div>
     </body>
